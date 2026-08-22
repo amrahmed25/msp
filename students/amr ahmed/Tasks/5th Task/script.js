b9a07ee16dd0eb17520bb89c023886ac7020e304
amrahmed25 <aamr46589@gmail.com>
2026-08-22 03:39:22 +0300

    .

---
+++ students/amr ahmed/Tasks/5th Task/script.js
@@ -0,0 +1,29 @@
+const loginForm = document.getElementById("loginForm");
+
+if (loginForm) {
+
+    loginForm.addEventListener("submit", function (event) {
+
+        event.preventDefault();
+
+       
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        const destination = prompt(
+            "What is your favorite travel destination?"
+        );
+        alert(
+            `Welcome ${name}! We hope you enjoy your trip to ${destination}.`
+        );
+        console.log("User Information:");
+        console.log("Name:", name);
+        console.log("Email:", email);
+        console.log("Password:", password);
+        console.log("Favorite Destination:", destination);
+        window.location.href = "index.html";
+        alert(
+            `Welcome ${name}! We hope you enjoy your trip to ${destination}.`
+        );
+    });
+}
