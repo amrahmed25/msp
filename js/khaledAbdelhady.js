4d6a8a28a9db36e5708ee893abab542509cf302e
khaled3bdo <khaled3bdelhady18@gmail.com>
2026-08-26 23:40:25 +0300

    rdsgfsd

--- js/khaledAbdelhady.js
+++ js/khaledAbdelhady.js
@@ -5,18 +5,18 @@ const khaledAbdelhady={
     tasksPath: "./students/Khaled3bdelhady/",
     assignments: [
       {
-        title: "Task 1 — RECIPE CARD",
-        folder: "./students/Khaled3bdelhady/Tasks/1stTask/",
-        preview: "./students/Khaled3bdelhady/Tasks/1stTask/PERFUME.jpeg",
-        description: "A Simple Perfume Card.",
+        title: "Task 1 — My favourite food ",
+        folder: "./students/Khaled3bdelhady/task1/",
+        preview: "./students/Khaled3bdelhady/task1/IMG.jpg",
+        description: "My favourite food.",
         technologies: ["HTML", "CSS"],
         github: "https://github.com/amrahmed25/msp/tree/main/students/Khaled3bdelhady/task1"
       },
 
       {
         title: "Task 2 — RECIPE CARD",
-        folder: "./students/Khaled3bdelhady/Tasks/2nd Task/",
-        preview: "./students/Khaled3bdelhady/Tasks/2nd Task/Eggs.jpeg",
+        folder: "./students/Khaled3bdelhady/task 2/",
+        preview: "./students/Khaled3bdelhady/task 2/img.jpeg/",
         description: "A Simple Recipe Card",
         technologies: ["HTML", "CSS"],
         github: "https://github.com/amrahmed25/msp/tree/main/students/Khaled3bdelhady/task%202"
@@ -24,8 +24,8 @@ const khaledAbdelhady={
 
       {
         title: "Task 3— Profiles Cards",
-        folder: "./students/Khaled3bdelhady/Tasks/3rd Task/",
-        preview: "./students/Khaled3bdelhady/Tasks/3rd Task/Images/khaled.jpeg",
+        folder: "./students/Khaled3bdelhady/task 3/",
+        preview: "./students/Khaled3bdelhady/task 3/images (1).jfif/",
         description: "A Simple Profiles with Cards",
         technologies: ["HTML", "CSS"],
         github: "https://github.com/amrahmed25/msp/tree/main/students/Khaled3bdelhady/task%203"
@@ -33,17 +33,17 @@ const khaledAbdelhady={
 
       {
         title: "Task 4 — Portfolio Landing Page",
-        folder: "./students/Khaled3bdelhady/Tasks/4th Task/",
-        preview: "./students/Khaled3bdelhady/Tasks/4th Task/img/khaled.jpeg",
-        description: "A responsive personal portfolio landing page with a hero section, project grid, and contact form.",
+        folder: "./students/Khaled3bdelhady/portfolio/",
+        preview: "./students/Khaled3bdelhady/portfolio/img2.jfif/",
+        description: "A responsive personal portfolio ",
         technologies: ["HTML", "CSS", "JavaScript"],
         github: "https://github.com/amrahmed25/msp/tree/main/students/Khaled3bdelhady/portfolio"
       },
 
       {
         title: "Task 5 — Travel website",
-        folder: "./students/Khaled3bdelhady/Tasks/5th Task/",
-        preview: "./students/Khaled3bdelhady/Tasks/5th Task/rome.jpg",
+        folder: "./students/Khaled3bdelhady/Travel Agency/",
+        preview: "./students/Khaled3bdelhady/Travel Agency/01_login_background.png/",
         description: "A responsive website with a hero section, project grid, and contact form.",
         technologies: ["HTML", "CSS", "JavaScript"],
         github: "https://github.com/amrahmed25/msp/tree/main/students/Khaled3bdelhady/Travel%20Agency"
@@ -57,6 +57,7 @@ const khaledAbdelhady={
         technologies: ["HTML", "CSS", "JavaScript"],
         github: ""
       },
+      
     ]
   };
   export default khaledAbdelhady;
