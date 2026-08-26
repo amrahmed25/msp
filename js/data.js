a045c24dff8f2b94f480a799cba37f47271a4e0f
KHALED-AMR-7777 <11410120250211@stud.cu.edu.eg>
2026-08-26 22:57:47 +0300

    done

--- js/data.js
+++ js/data.js
@@ -1,10 +1,12 @@
 
 import amrAhmed from "./amrAhmed.js";
 import khalidAmr from "./khalidAmr.js";
+import khaledAbdelhady from "./khaledAbdelhady.js";
 
   const students = [
     amrAhmed,
     khalidAmr,
+    khaledAbdelhady,
 
   ];
 
