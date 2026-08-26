f41e1d91afba57524c96d071feb410b2d160ae29
KHALED-AMR-7777 <11410120250211@stud.cu.edu.eg>
2026-08-26 04:20:48 +0300

    sorry

--- js/khalidAmr.js
+++ js/khalidAmr.js
@@ -52,8 +52,8 @@ const khalidAmr={
       {
         title: "Quiz — STUDENT ACTIVITY-MSP ",
         folder: "./students/khalidAmr/Tasks/QUIZ/",
-        preview: "./students/khalidAmr/Tasks/5th Task/khaled.jpeg",
-        description: "A responsive website with a Home section, Abou section, and contact.",
+        preview: "./students/khalidAmr/Tasks/QUIZ/khaled.jpeg",
+        description: "A responsive website with a Home section, About section, and contact.",
         technologies: ["HTML", "CSS", "JavaScript"],
         github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/QUIZ"
       },
