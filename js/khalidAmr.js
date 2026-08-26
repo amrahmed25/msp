4e243244709208c128e7f217248ecd458c786434
KHALED-AMR-7777 <11410120250211@stud.cu.edu.eg>
2026-08-26 04:04:31 +0300

    Done

--- js/khalidAmr.js
+++ js/khalidAmr.js
@@ -5,9 +5,17 @@ const khalidAmr={
     tasksPath: "./students/khalidAmr/Tasks/",
     assignments: [
       {
+        title: "Quiz — STUDENT ACTIVITY-MSP ",
+        folder: "./students/khalidAmr/Tasks/5th Task/",
+        preview: "./students/khalidAmr/Tasks/5th Task/khaled.jpeg",
+        description: "A responsive website with a Home section, Abou section, and contact.",
+        technologies: ["HTML", "CSS", "JavaScript"],
+        github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/QUIZ"
+      },
+      {
         title: "Task 5 — Travel website",
         folder: "./students/khalidAmr/Tasks/5th Task/",
-        preview: "./students/khalidAmr/Tasks/5th Task/images/bali.jpg",
+        preview: "./students/khalidAmr/Tasks/5th Task/rome.jpg",
         description: "A responsive website with a hero section, project grid, and contact form.",
         technologies: ["HTML", "CSS", "JavaScript"],
         github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/5th%20Task"
@@ -15,34 +23,34 @@ const khalidAmr={
       {
         title: "Task 4 — Portfolio Landing Page",
         folder: "./students/khalidAmr/Tasks/4th Task/",
-        preview: "./students/khalidAmr/Tasks/4th Task/img/amr3.png",
+        preview: "./students/khalidAmr/Tasks/4th Task/img/khaled.jpeg",
         description: "A responsive personal portfolio landing page with a hero section, project grid, and contact form.",
         technologies: ["HTML", "CSS", "JavaScript"],
         github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/4th%20Task"
       },
       {
-        title: "Task 3— auto car",
+        title: "Task 3— Profiles Cards",
         folder: "./students/khalidAmr/Tasks/3rd Task/",
-        preview: "./students/khalidAmr/Tasks/3rd Task/img/amg.jpg",
-        description: "Modern car showcase with responsive design.",
-        technologies: ["HTML", "CSS", "JavaScript"],
+        preview: "./students/khalidAmr/Tasks/3rd Task/Images/khaled.jpeg",
+        description: "A Simple Profiles with Cards",
+        technologies: ["HTML", "CSS"],
         github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/3rd%20Task"
       },
       {
-        title: "Task 2 — restaurant",
+        title: "Task 2 — RECIPE CARD",
         folder: "./students/khalidAmr/Tasks/2nd Task/",
-        preview: "./students/khalidAmr/Tasks/2nd Task/rest.png",
-        description: "Modern restaurant website with menu and reservations.",
-        technologies: ["HTML", "CSS", "JavaScript"],
+        preview: "./students/khalidAmr/Tasks/2nd Task/Eggs.jpeg",
+        description: "A Simple Recipe Card",
+        technologies: ["HTML", "CSS"],
         github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/2nd%20Task"
       },
       
       {
-        title: "Task 1 — restaurant",
+        title: "Task 1 — PERFUME CARD",
         folder: "./students/khalidAmr/Tasks/1stTask/",
-        preview: "./students/khalidAmr/Tasks/1stTask/preview.jpeg",
-        description: "Modern restaurant website with menu and reservations.",
-        technologies: ["HTML", "CSS", "JavaScript"],
+        preview: "./students/khalidAmr/Tasks/1stTask/PERFUME.jpeg",
+        description: "A Simple Perfume Card.",
+        technologies: ["HTML", "CSS"],
         github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/1st%20Task"
       }
     ]
