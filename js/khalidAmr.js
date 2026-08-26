b87fc7dd3491bbd3ad2fab43af2f72696bad15d6
KHALED-AMR-7777 <11410120250211@stud.cu.edu.eg>
2026-08-26 04:16:32 +0300

    done

--- js/khalidAmr.js
+++ js/khalidAmr.js
@@ -1,41 +1,18 @@
 const khalidAmr={
     id: "khalidAmr",
     name: "khaled Amr",
-    image: "./students/khalidAmr/image/amr3.png",
+    image: "./students/khalidAmr/Image/khaled2.jpeg",
     tasksPath: "./students/khalidAmr/Tasks/",
     assignments: [
       {
-        title: "Quiz — STUDENT ACTIVITY-MSP ",
-        folder: "./students/khalidAmr/Tasks/5th Task/",
-        preview: "./students/khalidAmr/Tasks/5th Task/khaled2.jpeg",
-        description: "A responsive website with a Home section, Abou section, and contact.",
-        technologies: ["HTML", "CSS", "JavaScript"],
-        github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/QUIZ"
-      },
-      {
-        title: "Task 5 — Travel website",
-        folder: "./students/khalidAmr/Tasks/5th Task/",
-        preview: "./students/khalidAmr/Tasks/5th Task/rome.jpg",
-        description: "A responsive website with a hero section, project grid, and contact form.",
-        technologies: ["HTML", "CSS", "JavaScript"],
-        github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/5th%20Task"
-      },
-      {
-        title: "Task 4 — Portfolio Landing Page",
-        folder: "./students/khalidAmr/Tasks/4th Task/",
-        preview: "./students/khalidAmr/Tasks/4th Task/img/khaled.jpeg",
-        description: "A responsive personal portfolio landing page with a hero section, project grid, and contact form.",
-        technologies: ["HTML", "CSS", "JavaScript"],
-        github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/4th%20Task"
-      },
-      {
-        title: "Task 3— Profiles Cards",
-        folder: "./students/khalidAmr/Tasks/3rd Task/",
-        preview: "./students/khalidAmr/Tasks/3rd Task/Images/khaled.jpeg",
-        description: "A Simple Profiles with Cards",
+        title: "Task 1 — PERFUME CARD",
+        folder: "./students/khalidAmr/Tasks/1stTask/",
+        preview: "./students/khalidAmr/Tasks/1stTask/PERFUME.jpeg",
+        description: "A Simple Perfume Card.",
         technologies: ["HTML", "CSS"],
-        github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/3rd%20Task"
+        github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/1stTask"
       },
+
       {
         title: "Task 2 — RECIPE CARD",
         folder: "./students/khalidAmr/Tasks/2nd Task/",
@@ -44,15 +21,42 @@ const khalidAmr={
         technologies: ["HTML", "CSS"],
         github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/2nd%20Task"
       },
+
+      {
+        title: "Task 3— Profiles Cards",
+        folder: "./students/khalidAmr/Tasks/3rd Task/",
+        preview: "./students/khalidAmr/Tasks/3rd Task/Images/khaled.jpeg",
+        description: "A Simple Profiles with Cards",
+        technologies: ["HTML", "CSS"],
+        github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/3rd%20Task"
+      },
+
+      {
+        title: "Task 4 — Portfolio Landing Page",
+        folder: "./students/khalidAmr/Tasks/4th Task/",
+        preview: "./students/khalidAmr/Tasks/4th Task/img/khaled.jpeg",
+        description: "A responsive personal portfolio landing page with a hero section, project grid, and contact form.",
+        technologies: ["HTML", "CSS", "JavaScript"],
+        github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/4th%20Task"
+      },
+
+      {
+        title: "Task 5 — Travel website",
+        folder: "./students/khalidAmr/Tasks/5th Task/",
+        preview: "./students/khalidAmr/Tasks/5th Task/rome.jpg",
+        description: "A responsive website with a hero section, project grid, and contact form.",
+        technologies: ["HTML", "CSS", "JavaScript"],
+        github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/5th%20Task"
+      },
       
       {
-        title: "Task 1 — PERFUME CARD",
-        folder: "./students/khalidAmr/Tasks/1stTask/",
-        preview: "./students/khalidAmr/Tasks/1stTask/PERFUME.jpeg",
-        description: "A Simple Perfume Card.",
-        technologies: ["HTML", "CSS"],
-        github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/1st%20Task"
-      }
+        title: "Quiz — STUDENT ACTIVITY-MSP ",
+        folder: "./students/khalidAmr/Tasks/5th Task/",
+        preview: "./students/khalidAmr/Tasks/5th Task/khaled2.jpeg",
+        description: "A responsive website with a Home section, Abou section, and contact.",
+        technologies: ["HTML", "CSS", "JavaScript"],
+        github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/QUIZ"
+      },
     ]
   };
   export default khalidAmr;
