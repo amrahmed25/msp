a834845fffed293e1081682f5ff11daa128c2749
KHALED-AMR-7777 <11410120250211@stud.cu.edu.eg>
2026-08-31 20:44:49 +0300

    fgklndfkghnfrdhi

--- js/Yousef-Sameh.js
+++ js/Yousef-Sameh.js
@@ -1,71 +1,55 @@
-const khalidAmr={
-    id: "khalidAmr",
-    name: "khaled Amr",
-    image: "./students/khalidAmr/Image/khaled.jpeg",
-    tasksPath: "./students/khalidAmr/Tasks/",
+const YousefSameh={
+    id: "YousefSameh",
+    name: "Yousef-Sameh",
+    image: "./students/YousefSameh/Image/khaled.jpeg",
+    tasksPath: "./students/YousefSameh/Tasks/",
     assignments: [
       {
         title: "Task 1 — PERFUME CARD",
-        folder: "./students/khalidAmr/Tasks/1stTask/",
-        preview: "./students/khalidAmr/Tasks/1stTask/PERFUME.jpeg",
+        folder: "./students/YousefSameh/Tasks/1stTask/",
+        preview: "./students/YousefSameh/Tasks/1stTask/PERFUME.jpeg",
         description: "A Simple Perfume Card.",
         technologies: ["HTML", "CSS"],
-        github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/1stTask"
+        github: "https://github.com/amrahmed25/msp/tree/main/students/YousefSameh/Tasks/1stTask"
       },
 
       {
         title: "Task 2 — RECIPE CARD",
-        folder: "./students/khalidAmr/Tasks/2nd Task/",
-        preview: "./students/khalidAmr/Tasks/2nd Task/Eggs.jpeg",
+        folder: "./students/YousefSameh/Tasks/2nd Task/",
+        preview: "./students/YousefSameh/Tasks/2nd Task/Eggs.jpeg",
         description: "A Simple Recipe Card",
         technologies: ["HTML", "CSS"],
-        github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/2nd%20Task"
+        github: "https://github.com/amrahmed25/msp/tree/main/students/YousefSameh/Tasks/2nd%20Task"
       },
 
       {
         title: "Task 3— Profiles Cards",
-        folder: "./students/khalidAmr/Tasks/3rd Task/",
-        preview: "./students/khalidAmr/Tasks/3rd Task/Images/khaled.jpeg",
+        folder: "./students/YousefSameh/Tasks/3rd Task/",
+        preview: "./students/YousefSameh/Tasks/3rd Task/Images/khaled.jpeg",
         description: "A Simple Profiles with Cards",
         technologies: ["HTML", "CSS"],
-        github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/3rd%20Task"
+        github: "https://github.com/amrahmed25/msp/tree/main/students/YousefSameh/Tasks/3rd%20Task"
       },
 
       {
         title: "Task 4 — Portfolio Landing Page",
-        folder: "./students/khalidAmr/Tasks/4th Task/",
-        preview: "./students/khalidAmr/Tasks/4th Task/img/khaled.jpeg",
+        folder: "./students/YousefSameh/Tasks/4th Task/",
+        preview: "./students/YousefSameh/Tasks/4th Task/img/khaled.jpeg",
         description: "A responsive personal portfolio landing page with a hero section, project grid, and contact form.",
         technologies: ["HTML", "CSS", "JavaScript"],
-        github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/4th%20Task"
+        github: "https://github.com/amrahmed25/msp/tree/main/students/YousefSameh/Tasks/4th%20Task"
       },
 
       {
         title: "Task 5 — Travel website",
-        folder: "./students/khalidAmr/Tasks/5th Task/",
-        preview: "./students/khalidAmr/Tasks/5th Task/rome.jpg",
+        folder: "./students/YousefSameh/Tasks/5th Task/",
+        preview: "./students/YousefSameh/Tasks/5th Task/rome.jpg",
         description: "A responsive website with a hero section, project grid, and contact form.",
         technologies: ["HTML", "CSS", "JavaScript"],
-        github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/5th%20Task"
+        github: "https://github.com/amrahmed25/msp/tree/main/students/YousefSameh/Tasks/5th%20Task"
       },
       
-      {
-        title: "Quiz — STUDENT ACTIVITY-MSP ",
-        folder: "./students/khalidAmr/Tasks/QUIZ/",
-        preview: "./students/khalidAmr/Tasks/QUIZ/khaled2.jpeg",
-        description: "A responsive website with a Home section, About section, and contact.",
-        technologies: ["HTML", "CSS", "JavaScript"],
-        github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/QUIZ"
-      },
-
-      {
-        title: "Task 7 - Platform Gaming  ",
-        folder: "./students/khalidAmr/Tasks/Project_gaming/",
-        preview: "./students/khalidAmr/Tasks/Project_gaming/Images/rdr2.jpg",
-        description: "A responsive platform gaming with a Home section, About section, category, feedback, and login section.",
-        technologies: ["HTML", "CSS", "JavaScript"],
-        github: "https://github.com/amrahmed25/msp/tree/main/students/khalidAmr/Tasks/Project_gaming"
-      },
+     
     ]
   };
-  export default khalidAmr;
+  export default YousefSameh;
