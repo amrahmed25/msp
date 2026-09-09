338c76728bc736177c692ed87abe4f00bd1f4ce7
khaled3bdo <khaled3bdelhady18@gmail.com>
2026-09-10 01:15:46 +0300

    gfgd

--- js/khaledAbdelhady.js
+++ js/khaledAbdelhady.js
@@ -73,7 +73,7 @@ const khaledAbdelhady={
         preview: "./students/Khaled3bdelhady/xo/bg.jfif/",
         description: "A responsive XO Game website ",
         technologies: ["HTML", "CSS", "JavaScript"],
-        github: ""
+        github: "https://github.com/amrahmed25/msp/tree/main/students/Khaled3bdelhady/xo"
       },
     ]
   };
