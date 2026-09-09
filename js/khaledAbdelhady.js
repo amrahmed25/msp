91dbd656c24c51a3502f5a20a68f84a766164e3c
khaled3bdo <khaled3bdelhady18@gmail.com>
2026-09-10 01:13:29 +0300

    gerg

--- js/khaledAbdelhady.js
+++ js/khaledAbdelhady.js
@@ -67,14 +67,8 @@ const khaledAbdelhady={
         github: "https://github.com/amrahmed25/msp/tree/main/students/Khaled3bdelhady/Project_gaming"
       },
 
-      {
-        title:"XO Game",
-        folder: "./students/Khaled3bdelhady/xo/",
-        preview: "./students/Khaled3bdelhady/xo/bg.jfif/",
-        description: "A responsive XO Game website ",
-        technologies: ["HTML", "CSS", "JavaScript"],
-        github: "https://github.com/amrahmed25/msp/tree/main/students/Khaled3bdelhady/xo"
-      },
+      
+
     ]
   };
   export default khaledAbdelhady;
