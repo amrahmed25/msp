4026591667012ad332728e4e130469c1f77905a0
Khaled <khlaed3bdelhady18@gmail.com>
2026-09-17 02:36:17 +0300

    gdfgfd

--- js/khaledAbdelhady.js
+++ js/khaledAbdelhady.js
@@ -75,13 +75,15 @@ const khaledAbdelhady={
         technologies: ["HTML", "CSS", "JavaScript"],
         github: "https://github.com/amrahmed25/msp/tree/main/students/Khaled3bdelhady/xo"
       },
+
+      
       {
         title:"Quiz Game",
         folder: "./students/Khaled3bdelhady/quiz game/",
         preview: "./students/Khaled3bdelhady/quiz game/Neon Castle Above the Misty Valley.png/",
         description: "A responsive QUIZ Game website ",
         technologies: ["HTML", "CSS", "JavaScript"],
-        github: ""
+        github: "https://github.com/amrahmed25/msp/tree/main/students/Khaled3bdelhady/quiz%20game"
       },
     ]
   };
