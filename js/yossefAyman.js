d1e651b45d968ebb615186b8e9f89f87648c2373
Yossef Ayman <aa6024726@gmail.com>
2026-09-16 23:55:34 +0300

    add: Quiz Game

--- js/yossefAyman.js
+++ js/yossefAyman.js
@@ -18,7 +18,14 @@ const YossefAyman={
             preview: "./students/YossefAyman/Tasks/XO/Images/XO.jpg",
             description: "A Tic Tac Toe game with an AI opponent, featuring three difficulty levels and a responsive design.",
             technologies: ["HTML", "tailwind", "JavaScript"],
-        }
+        },
+        {
+            title: "Quiz Game",
+            folder: "./students/YossefAyman/Tasks/Quiz-App/",
+            preview: "./students/YossefAyman/Tasks/Quiz-App/Images/Quiz-App.jpeg",
+            description: "An interactive Quiz App with configurable categories, difficulty levels, question limits, and countdown timers.",
+            technologies: ["HTML", "BootStrap", "Css", "JavaScript"],
+        },
     ]
 };
 export default YossefAyman;
